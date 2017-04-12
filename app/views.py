4d5d29b8ca7deca64de1d53a2c6d4bc7862996f3
Sumitha Dhanasekaran <icanfreaku@gmail.com>
2017-04-12 10:55:19 +0530

    GitHub api integrated
    first commit

---
+++ app/views.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, HttpResponse
+import json
+import requests
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse('Hello Sumita!')
+
+def test(request):
+    return HttpResponse('You are in the test page')
+
+def profile(request):
+    jsonList = []
+    req = requests.get('https://api.github.com/users/DrkSephy')
+    jsonList.append(json.loads(req.content))
+    parsedData = []
+    userData = {}
+    for data in jsonList:
+    userData['name'] = data['name']
+        userData['blog'] = data['blog']
+        userData['email'] = data['email']
+        userData['public_gists'] = data['public_gists']
+        userData['public_repos'] = data['public_repos']
+        userData['avatar_url'] = data['avatar_url']
+        userData['followers'] = data['followers']
+        userData['following'] = data['following']
+    parsedData.append(userData)
+    return HttpResponse(parsedData)
